dd235fb667353dd243f2cf0cf2c1e254cc25a2ff
luizphs14 <marques.luiz@outlook.com>
2021-12-02 23:51:48 -0300

    Botao-remover-limpar02

--- src/data/database.js
+++ src/data/database.js
@@ -2,6 +2,6 @@ export const produtos = [
     {url: "https://picsum.photos/200/200?a=3", name: "Meteorito", preco: 50 },
     {url: "https://picsum.photos/200/200?a=2", name: "Foguete", preco: 150 },
     {url: "https://picsum.photos/200/200?a=4", name: "Roupa espacial", preco: 250 },
-    {url: "https://picsum.photos/200/200?a=5", name: "Satélite antigo", preco: 350 },
+    {url: "https://picsum.photos/200/200?a=5", name: "Satélite antigo", preco: 450 },
 ];
 
